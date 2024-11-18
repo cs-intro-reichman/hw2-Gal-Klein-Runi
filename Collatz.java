3031d983cc726eaaf07a1808b88e04df4f351d51
Gal Klein <gal.klein@post.runi.ac.il>
2024-11-18 10:29:19 +0200

    HomeWork2

--- Collatz.java
+++ Collatz.java
@@ -2,5 +2,36 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		String mode = args[0];
+		int N = Integer.parseInt(args[1]);
+		String summeryLine = "Every one of the first "+N+" hailstone sequences reached 1.";
+		if (mode.equals("v")) {
+		for (int i=1; i<N+1; i++) {
+			int counter = 1;
+			int num = i;
+			String printLine = ""+num;
+			if (num%2==0) {
+				num = num/2;
+				printLine = printLine+" "+num;
+			} else {
+				num = num*3 +1;
+				printLine = printLine+" "+num;
+			}
+			counter ++;
+			while (num!=1) {
+				if (num%2==0) {
+					num = num/2;
+					printLine = printLine+" "+num;
+				} else {
+					num = num*3 +1;
+					printLine = printLine+" "+num;
+				}
+				counter ++;
+			}
+			printLine = printLine+" "+"("+counter+")";
+			System.out.println(printLine);
+			}}
+			System.out.println(summeryLine);
+		}
 	}
-}
+
